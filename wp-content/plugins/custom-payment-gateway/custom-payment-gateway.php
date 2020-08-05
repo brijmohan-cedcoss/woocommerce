c9c3f9948abdee539f7cd7696d1d112b74df19c2
brijmohan-cedcoss <brijmohanpandey@cedcoss.com>
2020-08-05 10:22:20 +0530

    branch update

--- wp-content/plugins/custom-payment-gateway/custom-payment-gateway.php
+++ wp-content/plugins/custom-payment-gateway/custom-payment-gateway.php
@@ -17,7 +17,7 @@
  */
 
 /**
- * Check if WooCommerce is active
+ * Check if WooCommerce is active.
  **/
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
 
